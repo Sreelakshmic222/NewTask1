fc2b1f79108886ded6b084d139ebb0b613435efb
Sreelakshmic222 <sreelakshmic222@gmail.com>
2023-04-22 23:23:13 +0530

    sec

--- restapi/api/urls.py
+++ restapi/api/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns=[
     # path('api/register',RegisterApi.as_view()),
     path('regi/',RegisterAPIView.as_view(),name='regi'),
-    path('log/',LoginAPIView.as_view(),name='log'),
+    path('log/',LoginAPIView.as_view(),name='logg'),
     path('register/',views.register,name='reg'),
     path('login/',views.login,name='log')
 ]
--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -17,16 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.urls import path, include
+from api.views import HomePageView
 from rest_framework_simplejwt import views as jwt_views
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
+    path('', HomePageView.as_view(), name='home'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
