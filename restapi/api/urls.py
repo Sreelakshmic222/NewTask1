0ac9aee531482a80f474579b398a0705eb9545c0
Sreelakshmic222 <sreelakshmic222@gmail.com>
2023-04-22 21:02:46 +0530

    mytaskcommit

---
+++ restapi/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from .api import RegisterApi
+from .views import RegisterAPIView,LoginAPIView,register,login
+from . import views
+urlpatterns=[
+    # path('api/register',RegisterApi.as_view()),
+    path('regi/',RegisterAPIView.as_view(),name='regi'),
+    path('log/',LoginAPIView.as_view(),name='log'),
+    path('register/',views.register,name='reg'),
+    path('login/',views.login,name='log')
+]
---
+++ restapi/api/views.py
@@ -0,0 +1,96 @@
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from rest_framework_jwt.settings import api_settings
+from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+class RegisterAPIView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+
+        if not username or not password or not email:
+            return Response({'error': 'Please provide username, email and password'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        # generate JWT token
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        return Response({'token': token}, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # generate JWT token
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        return Response({'token': token}, status=status.HTTP_200_OK)
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         # handle registration form submission
+#         ...
+#     else:
+#         # render registration form template
+#         return render(request, 'register.html')
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         # handle login form submission
+#         ...
+#     else:
+#         # render login form template
+#         return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # redirect to success page
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            # authenticate user and redirect to success page
+            pass
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+class HomePageView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'homepage.html'
+
+    def get(self, request):
+        # Your view logic here
+        return Response({'message':'homepage'})
